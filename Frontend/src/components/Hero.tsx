a5785709e75ee086e7aa569bba131c8f0d9f08a8
shivam kevat <webhunter81@gmail.com>
2024-09-23 23:16:57 +0530

    first

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -1,14 +1,18 @@
 const Hero = () => {
   return (
-    <div className="bg-blue-800 pb-16 animate__animated animate__fadeIn">
+    <div className="bg-white pb-16 animate__animated animate__fadeIn flex justify-between ">
       <div className="container mx-auto flex flex-col gap-2">
-        <h1 className="text-5xl text-white font-bold tracking-wide animate__animated animate__fadeInDown">
+        <h1 className="text-5xl text-black font-bold tracking-wide animate__animated animate__fadeInDown">
           Find your next stay
         </h1>
-        <p className="text-2xl text-white animate__animated animate__fadeInUp">
+        <p className="text-2xl text-black animate__animated animate__fadeInUp">
           Search low prices on hotels for your dream vacation...
         </p>
       </div>
+      <div className="p-10 mr-10 w-9/12">
+        <img src="..\..\src\assets\heroImg.png" alt=""/>
+      </div>
+      
     </div>
   );
 };
